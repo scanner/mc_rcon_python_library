65d4ff1050de9def8cd3691c03142ea6c73ce888
Scanner <scanner@apricot.com>
2014-08-14 19:57:22 -0700

    Use readline. Hardcode test host for now.

--- repl.py
+++ repl.py
@@ -9,27 +9,37 @@ simple repl console loop
 # system imports
 #
 import getpass
-
+import readline
+import os.path
 # Our imports
 #
 from mcrcon import mcrcon
 
 print 'Ctrl-C to exit'
-host = raw_input('Host: ')
-port = raw_input('Port (25575): ')
-if port == '':
-    port = 25575
-else:
-    port = int(port)
-pwd = getpass.getpass('Password: ')
+# host = raw_input('Host: ')
+# port = raw_input('Port (25575): ')
+# if port == '':
+#     port = 25575
+# else:
+#     port = int(port)
+# pwd = getpass.getpass('Password: ')
 
 print "Connecting..."
-r = mcrcon.MCRcon(host, port, pwd)
+r = mcrcon.MCRcon("soujya.apricot.com", 25575, "foobybooby")
 print "Logged in successfully"
+
+# Read our history file for readline, if it exists.
+#
+histfile = os.path.join(os.environ["HOME"], ".mcrepl")
+try:
+    readline.read_history_file(histfile)
+except IOError:
+    pass
 
 try:
     while True:
         line = raw_input('Rcon: ')
         print r.send(line)
 except KeyboardInterrupt, e:
+    readline.write_history_file(histfile)
     r.close()
